d6b2ff8e8fdbbab0c97a34e5c0743a7cf02b5872
MBSoo <qud9783@khu.ac.kr>
2024-12-10 17:15:22 +0900

    [fix] remote 파일 저장 경로 수정

--- src/main/java/Manager/spring/service/CardService.java
+++ src/main/java/Manager/spring/service/CardService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -59,12 +60,26 @@ public class CardService {
     }
 
     public String saveImage(MultipartFile image, String uploadsDir) throws IOException {
+        String projectBasePath = System.getProperty("user.home") + "/Manager/Manager-Backend";
+
+        // 업로드 디렉토리 경로 설정
+        String uploadDir = projectBasePath + "/src/main/resources/static/uploads/";
+
+        // 디렉토리 생성
+        File directory = new File(uploadDir);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+
+        // 고유한 파일명 생성
         String fileName = UUID.randomUUID().toString().replace("-", "") + "_" + image.getOriginalFilename();
-        String filePath = uploadsDir + fileName;
+        String filePath = uploadDir + fileName;
+
+        // DB에 저장될 파일 경로
         String dbFilePath = "/api/v1/card/uploads/" + fileName;
 
+        // 파일 저장
         Path path = Paths.get(filePath);
-        Files.createDirectories(path.getParent());
         Files.write(path, image.getBytes());
 
         return dbFilePath;
